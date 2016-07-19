ec88f7cfee592ff1e03d572a803d87201ef9f031
Chris Sparshott <idioteque23@gmail.com>
2016-07-19 22:00:59 +0100

    toggle post if desired. heading and search terms moved ready for config

--- content.js
+++ content.js
@@ -1,8 +1,16 @@
-const SN_FACEBOOK = 'facebook';
-const SN_TWITTER = 'twitter';
+var headingText = 'Pokemon Go Away!';
 const SEARCH_TERMS = [
   'pokemon', 'pokémon'
 ];
+
+/**
+ * All code above is config
+ * all code below is extension related.
+ *
+ */
+
+const SN_FACEBOOK = 'facebook';
+const SN_TWITTER = 'twitter';
 
 var hostname = window.location.hostname;
 var social_networks = [
@@ -15,19 +23,29 @@ function removeShit()
   var output = $(
     '<div>',
     {
-      'style': 'background: #eee; padding: 50px 20px;',
-      'class': 'js-unshit-parent'
+      'style': 'background: #eee; padding: 20px 20px; overflow: hidden; border: 1px solid rgba(0, 0, 0, 0.15);',
+      'class': 'js-post-parent'
     }
   );
   var heading = $(
     '<h1>',
     {
-      'style': 'color: #666;',
-      'text':  'Pokemon Go Away!'
+      'style': 'color: #666; margin-top: 8px;',
+      'text':  headingText
     }
   );
+  var btn = $(
+    '<button>',
+    {
+      'class': 'js-post-toggle',
+      'text':  'Show Post',
+      'style': 'float: right; padding: 8px 12px; border: 1px solid rgba(0, 0, 0, 0.25); border-radius: 2px; background: #3B5998; color: rgba(255, 255, 255, 0.75); cursor: pointer;'
+    }
+  );
+  output.append(btn);
+  output.append(heading);
 
-  return output.append(heading);
+  return output;
 }
 
 function hidePost(container, parentSelector, searchTerm)
@@ -38,11 +56,20 @@ function hidePost(container, parentSelector, searchTerm)
       var $this = $(this),
         str = $.trim($this.text().toLowerCase());
 
+      if($this.closest(parentSelector).hasClass('js-processed'))
+      {
+        return false;
+      }
+
       if(str.length > 0 && $this.is(':visible'))
       {
         if(str.indexOf(searchTerm) > -1)
         {
-          $this.closest(parentSelector).replaceWith(removeShit(parentSelector));
+          var item = $this.closest(parentSelector)
+                          .addClass('js-processed')
+                          .hide();
+
+          removeShit().insertBefore(item);
         }
       }
     }
@@ -93,3 +120,23 @@ function DOMModificationHandler()
 
 //after document-load
 $(document).on('DOMSubtreeModified.event1', DOMModificationHandler);
+
+$(document).on(
+  'click', '.js-post-toggle', function ()
+  {
+    var post = $(this).closest('.js-post-parent')
+                      .parent()
+                      .find('.js-processed');
+
+    post.toggle();
+
+    if(post.is(':visible'))
+    {
+      $(this).text('Close Post');
+    }
+    else
+    {
+      $(this).text('Show Post');
+    }
+  }
+);
